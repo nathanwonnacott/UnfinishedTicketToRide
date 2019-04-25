dd2660ea72bcb219685a3712f67fe6b0edcf4eb6
Nate Wonnacott <nathanwonnacott@gmail.com>
2019-04-24 21:33:13 -0700

    Minor mods to signature and comments in GameController

--- TicketToRide/src/tickettoride/GameController.java
+++ TicketToRide/src/tickettoride/GameController.java
@@ -1,15 +1,17 @@
 package tickettoride;
 
-import java.util.Set;
-
+import java.util.Map;
 import tickettoride.model.MapData;
 import tickettoride.players.Player;
 
 /**
  * This is the class that handles the main game flow.
  * At this point, this class is obviously far from done.
- * Students may need to edit this file, but I wouldn't recommend editing it
- * yet because there will likely be some more changes produced Nate
+ * This is really just a stub to get students on a potential right
+ * track, but since it is not directly related to the Player interface or
+ * any other interfaces that are required to remain unchanged, students are
+ * free to modify this file however they want, or even get rid of it if they
+ * prefer to handle the game flow elsewhere.
  * @author nate
  */
 public class GameController {
@@ -17,10 +19,13 @@ public class GameController {
 	/**
 	 * Instantiates a game controller object
 	 * @param map map data to represent the game map state
-	 * @param players Set of players participating in the game
+	 * @param players map from player descriptions to players participating
+	 * in the game. The player description could be as simple as "Player 1", 
+	 * "Player 2", etc, or could be an actual name, or description of player type
+	 * combined with a number or name.
 	 */
-	public GameController(MapData map, Set<? extends Player> players) {
-		
+	public GameController(MapData map, Map<String, ? extends Player> players) {
+		//TODO to be implemented by student
 		
 	}
 	
@@ -28,6 +33,6 @@ public class GameController {
 	 * This is the main game loop
 	 */
 	public void runGame() {
-		
+		//TODO to be implemented by student
 	}
 }
