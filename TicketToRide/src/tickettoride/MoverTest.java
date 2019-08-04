86ee12ea0603a89de3029b19209def67aa99e842
Nate Wonnacott <nathanwonnacott@gmail.com>
2019-08-03 22:26:58 -0700

    Change build path order, add junit5 to pom, remove junit 4 assertThat

--- TicketToRide/src/tickettoride/MoverTest.java
+++ TicketToRide/src/tickettoride/MoverTest.java
@@ -1,6 +1,6 @@
 package tickettoride;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalMatchers.*;
 import static org.mockito.Matchers.*;
